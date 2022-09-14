0956b6b5cf39cfc6af8f7d4ebaca9927a1d08fbe
NishanthSanjeevi <57482332+NishanthSanjeevi@users.noreply.github.com>
2022-09-15 02:49:30 +0530

    Add solution for 1105, coded in C++

---
+++ dc1105.c
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    int sum = 0, halfsum = 0;
+    cin >> n;
+    vector <int> arr(n);
+    
+    // get input
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    
+    // If the sum is not even, we can not split it to 2 separate sub-arrays
+    if (sum % 2) {
+        cout << "Not possible\n";
+        return 0;
+    }
+
+    // We need to see if any sub-array can be equal to half the sum
+    // if it is then the remaining part would make the other sub-array
+    halfsum = sum / 2;
+    
+    // Create a boolean array of size halfsum
+    vector <bool> possibleSum (halfsum + 1, false);
+    
+    // Parse the array and set the array values to 1
+    for (int i = 0; i < n; i++) {
+        possibleSum[arr[i]] = true;
+    }
+    
+    // iterate through the array to form possible subarray sums
+    for (int i = 0; i < n; i++) {
+        for (int j = halfsum; j > arr[i]; j--) {
+            if (possibleSum[j - arr[i]]) {
+                possibleSum[j] = true;
+            }
+        }
+    }
+    
+    /*
+    for (int i = 0; i <= halfsum; i++) {
+        cout << possibleSum[i] << " ";
+    }
+    cout << endl;
+    */
+ 
+    cout << "Possibility to divide the array to equal subarrays: " << possibleSum[halfsum] << endl;
+
+    return 0;
+}
